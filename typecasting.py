9f7fefe15d5cc0fca3f81b763aa02044ab919ff7
shrik4 <159700715+shrik4@users.noreply.github.com>
2026-02-12 10:19:02 +0530

    typecasting

---
+++ typecasting.py
@@ -0,0 +1,11 @@
+"there are two types of type casting implicit typecasting and explicit typecating"
+a= 5
+b =2.5
+c= a + b
+print(c)
+print(type(c))
+
+# explicit typecasting
+num_str = "100"
+num_int = int(num_str)
+print(num_int + 50)
