de3790e4d0edf11357a4999a7f0e27c28dda6f22
shrik4 <159700715+shrik4@users.noreply.github.com>
2026-02-12 09:47:37 +0530

    internship

---
+++ list.py
@@ -0,0 +1,51 @@
+list = ['shrikar','arfain','sarthak']
+# list = [2]
+print(list[-2:-1])
+print(list[-1])
+list = ['shobith','shrikar','sarthak','arfain','anshul']
+
+
+#tuples
+tuple = (10,20,30,40,50)
+print(tuple[1])
+print(tuple[-1])
+print(tuple[0:4])
+print(tuple[-3:-1])
+
+
+# dict
+dict = {'key':'value','age':'20'}
+print(dict['key'])
+print(dict['age'])
+print(dict.keys())
+print(dict.values())
+
+# changing
+str = "hriyanka"
+str = ('P'+str[1:])
+print('P'+str[1:])
+
+print(str[0:3]+ 'y'+str[4: ])
+str1 = "i am in \'cotriver\' for intership"
+str2 = 'i am in \"contriver\" for intership'
+print(str1,'\n',str2)
+
+
+
+
+
+# assi
+
+# palindrom checker  program
+name = "mama"
+process_text = name.replace("","").lower()
+if process_text == process_text[::-1]:
+    process_text("is palindrom")
+else:
+    print("not palindrom")
+
+# 100 line code 
+
+# how do u take input from user in python 
+
+# what is typecasting give am example
