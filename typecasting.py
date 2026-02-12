f7d4c4366b534da5511651d4f4bf5bf11ae85a82
shrik4 <159700715+shrik4@users.noreply.github.com>
2026-02-12 10:38:20 +0530

    diff typescript

--- typecasting.py
+++ typecasting.py
@@ -6,6 +6,31 @@ print(c)
 print(type(c))
 
 # explicit typecasting
-num_str = "100"
-num_int = int(num_str)
-print(num_int + 50)
+# num_str = "100"
+# num_int = int(num_str)
+# print(num_int + 50)
+
+# num_str = "25"
+# num_int = int(num_str)
+# print((num_int + 5))
+# print(type(num_int))
+
+# num_str = "12.5"
+# num_float = float(num_str)
+# print(num_float)
+
+
+# num = 100
+# num_str = str(num)
+# print("value is " + num_str)
+
+
+# text = "shrikar"
+# textlist = list(text)
+# print(textlist)
+
+
+numbers = [1,2,3,4]
+text = tuple(numbers)
+print(text)
+print(type(text))
